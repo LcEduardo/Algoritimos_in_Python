9e54f8b36f68082093a099169ad1b45b3e9f3130
LcEduardo <lucasproject777@gmail.com>
2024-07-05 15:46:09 -0300

    Organizando CAP

---
+++ CAP4/Quicksort/quicksort.py
@@ -0,0 +1,10 @@
+def quicksort(array):
+    if len(array) < 2:
+        return array    
+    else:
+        pivo = array[0]
+        menores = [i for i in array[1:] if i <=pivo]
+        maiores = [i for i in array[1:] if i > pivo]
+        return quicksort(menores) + [pivo] + quicksort(maiores)
+    
+print(f'{quicksort([10, 5, 2, 3])}')
